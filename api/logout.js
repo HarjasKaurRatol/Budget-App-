b32adaf239609f92a499c31968861f11bd60c51d
HarjasKaurRatol <noor@Noors-MacBook-Pro.local>
2026-07-26 01:46:11 -0700

    Revert "Add password-gated auth"
    This reverts commit 46b78440fb1f13fd4e5197cb8e8e6a9077e04efa.

--- api/logout.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader("Set-Cookie", "app_auth=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax");
-  res.writeHead(302, { Location: "/login.html" });
-  res.end();
-};
